9532e3cf4c313fa7195ac4bd112195fb70f5f954
yseij <r0790156@student.thomasmore.be>
2022-02-10 19:09:05 +0100

    Dynamische routegegevens extraheren

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,9 @@
+import { useRouter } from "next/router";
+
 function DetailPage() {
+  const router = useRouter();
+  const newsId = router.query.newsId;
+  console.log(newsId);
   return <h1>The Detail Page</h1>;
 }
 
